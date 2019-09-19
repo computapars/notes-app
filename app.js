ad497ebbbb53af902d2477e74994533861219041
Marlene Bowles <mbowles@broadway.com>
2019-09-19 12:40:22 -0400

    Seolves challenge for section 3

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const fsPromises = require('fs').promises;
+/* Section 2 */
+// const fsPromises = require('fs').promises;
 
-async function writeTheFile () {
-    fsPromises.writeFile('Notes.txt', 'Initial comment on File');
-    await fsPromises.appendFile('Notes.txt', '. This is a New comment on the file');
-}
+// async function writeTheFile () {
+//     fsPromises.writeFile('Notes.txt', 'Initial comment on File');
+//     await fsPromises.appendFile('Notes.txt', '. This is a New comment on the file');
+// }
 
-writeTheFile();
+// writeTheFile();
+
+/* Section 3 */
+const getNotes = require('./notes.js');
+
+getNotes()
---
+++ notes.js
@@ -0,0 +1,5 @@
+const getNotes = () => {
+    console.log("Get's the notes");
+}
+
+module.exports = getNotes;
