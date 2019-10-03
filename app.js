49687b11553713dd562a4069f0f14559e503d547
Marlene Bowles <mbowles@broadway.com>
2019-10-03 16:27:19 -0400

    Finishes read command and refactors command names

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
-/* Section 2 
-// Using asynchronous file writing and appending
-const fsPromises = require('fs').promises;
-
-async function writeTheFile () {
-    fsPromises.writeFile('Notes.txt', 'Initial comment on File');
-    await fsPromises.appendFile('Notes.txt', '. This is a New comment on the file');
-}
-
-writeTheFile();
-*/
-
-/* Section 3
-// Requiring modules and importing them;
-
-const getNotes = require('./notes.js');
-getNotes()
-
-Using npm modules and using api's
-const chalk = require('chalk');
-console.log(chalk.green.bold.inverse('Success'));
-*/
-
-/* Section 4 
-// Using arguments in command line
-*/
-
 const yargs = require('yargs');
 const notes = require('./notes.js');
 yargs.version('1.0.2');
 
-// /* Sets up Yarg commands */
+/* Sets up Yarg commands */
 const notesObj = {
     title : {
         desc: 'Note Title',
@@ -39,35 +12,21 @@ const notesObj = {
     body : {
         desc: 'Note body',
         type: 'string',
-        demandOption: true,        
+        demandOption: false,        
     }
 };
 
-yargs.command('add', 'Adds a note', notesObj, (argv) => {
-        notes.addNotes(argv.title, argv.body);
+yargs.command('add', 'Adds a note',     {...notesObj, 
+    body: {
+        ...notesObj.body,
+        demandOption: true,
+    }}, (argv) => {
+        notes.add(argv.title, argv.body);
     },
 ).command('remove', 'Removes notes', notesObj, (argv) => {
-    notes.removeNote(argv.title);
-}).command('list', 'Lists all notes', () => {
-    console.log('listing a note')
-}).command('read', 'Reads the notes', () => {
-    console.log('Reading a note')
+    notes.remove(argv.title);
+}).command('listNotes', 'Lists all notes', () => {
+    notes.listNotes();
+}).command('read', 'Reads the notes', notesObj, (argv) => {
+    notes.read(argv.title)
 }).help().argv;
-
-
-/* Section 4 
-// Working with JSON and the file system
-*/
-
-const fs = require('fs');
-
-const dataBuffer = fs.readFileSync('playground/1-json.json');
-const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data)
-
-data.name = "Marlene";
-data.planet = "Virgo";
-data.age = "37";
-
-fs.writeFileSync('playground/1-json.json', JSON.stringify(data));
--- notes.js
+++ notes.js
@@ -1,19 +1,40 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = () => {
-    console.log("Get's the notes");
+// Utils
+const findMatchingNote = (title) => {
+    const notes = loadNotes();
+    return notes.find((item) => item.title === title);
+}
+const saveNotes = (noteArray) => {
+    const notesJSON = JSON.stringify(noteArray);
+    fs.writeFileSync('notes.json', notesJSON)
+};
+
+const loadNotes = () => {
+    try {
+        const notesBuffer = fs.readFileSync('notes.json');
+        const notesJSON = notesBuffer.toString();
+        return JSON.parse(notesJSON);    
+    } catch (error) {
+        return [];
+    }
 }
 
-const findDuplicates = (title) => {
-    const notes = loadNotes();
-    return notes.filter((item) => item.title === title).length > 0;
+// Methods
+const readNotes = (title) => {
+    const matchingNote = findMatchingNote(title);
+    if (matchingNote) {
+        console.log(chalk.green(matchingNote.title), matchingNote.body)
+    } else {
+        console.log(chalk.red('Error, no matching note found'))
+    }
 }
 
 const addNotes = (title, body) => {
     const notes = loadNotes();
-    const hasDuplicates = findDuplicates(title);
-    if (!hasDuplicates) {
+    const hasANoteAlready = findMatchingNote(title);
+    if (!hasANoteAlready) {
         notes.push({
             title: title,
             body: body,
@@ -36,24 +57,18 @@ const removeNote = (title) => {
     }
 }
 
-const saveNotes = (noteArray) => {
-    const notesJSON = JSON.stringify(noteArray);
-    fs.writeFileSync('notes.json', notesJSON)
-};
 
-const loadNotes = () => {
-    try {
-        const notesBuffer = fs.readFileSync('notes.json');
-        const notesJSON = notesBuffer.toString();
-        return JSON.parse(notesJSON);    
-    } catch (error) {
-        return [];
-    }
-    
+const listNotes = () => {
+    const notes = loadNotes();
+    console.log(chalk.green('Your Notes:'));
+    notes.forEach((note) => {
+        console.log(chalk.green.inverse(note.title));
+    })
 }
 
 module.exports = {
-    addNotes: addNotes,
-    getNotes: getNotes,
-    removeNote: removeNote,
+    add: addNotes,
+    remove: removeNote,
+    listNotes: listNotes,
+    read: readNotes,
 }
